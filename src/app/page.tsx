d7a11611e742670a190aa68b225b7b41c28c2745
Caleb Rogers <caleb@calebjay.com>
2023-07-25 17:59:26 +0800

    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,23 @@
 'use client'
 import LanguagePicker from '@/components/LanguagePicker';
 import { LanguagesEnglish } from '@/const';
+import  Typography  from '@mui/material/Typography';
+import  Box from '@mui/material/Box';
 import { useState } from 'react';
 
 export default function Home() {
   const [primaryLanguage, setPrimaryLanguage] = useState('');
   const [secondaryLanguage, setSecondaryLanguage] = useState('');
-  function handleChange(val: keyof typeof LanguagesEnglish | undefined) {
-  }
   return (
     <main className="flex min-h-screen flex-row justify-between p-24">
-      <section>
+      <Box sx={{width: '50%' }}>
+        <Typography variant="h6" gutterBottom>I speak...</Typography>
         <LanguagePicker onChange={(val => setPrimaryLanguage(val || '') )} id="primary-language" label="Native Language" />
-      </section>
-      <section>
+      </Box>
+      <Box sx={{ width: '50%' }}>
+        <Typography variant="h6" gutterBottom>I'm traveling somewhere that speaks...</Typography>
         <LanguagePicker onChange={(val => setSecondaryLanguage(val || ''))} id="secondary-language" label="Foreign Language" />
-      </section>
+      </Box>
     </main>
   )
 }
